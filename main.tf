2bea4150e90b9d6bc48a5be0f893c71aafbed3b4
John Tucker <jtucker@spotify.com>
2021-08-28 19:04:03 -0400

    fix

--- main.tf
+++ main.tf
@@ -18,6 +18,11 @@ terraform {
 
 # PROVIDER
 
+provider "kubectl" {
+  config_path    = "~/.kube/config"
+  config_context = var.kubernetes_config_context
+}
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = var.kubernetes_config_context
